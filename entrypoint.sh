c88a0a331ac308d6764cf5c3ec8d9f5a0f7acec4
Scott Paige <scottpaige1@gmail.com>
2020-02-22 15:25:24 -0700

    Updated task logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,6 @@ fly --target "${CONCOURSE_TEAM}" login \
   --password "${CONCOURSE_PASSWORD}"
 
 
-for file in ci/config/pipelines/aws-prod/*; do 
 
-    validator="$(fly -t ${CONCOURSE_TEAM} validate-pipeline -c ${file} )"
+fly -t ${CONCOURSE_TEAM} validate-pipeline -c ${PIPELINE_CONFIG}
 
-    if [[ $validator != *"looks good"* ]]; then
-        command || exit 1
-    else echo "Passed"
-    fi
-done
