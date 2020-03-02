48b0b371c31de36cface4f5d99fd637cfb57f1e3
Scott Paige <scottpaige1@gmail.com>
2020-03-02 14:18:55 -0700

    Debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,13 +18,16 @@ red=$'\e[1;31m'
 white=$'\e[0m'
 
 
-set -x 
-
 yq r --printMode p "${PIPELINE_CONFIG}" jobs[*].plan[*].file >> paths.yml
 while IFS= read -r line; do
     FILE="$(yq r ${PIPELINE_CONFIG} $line)"
     job_name="$(yq r ${PIPELINE_CONFIG} ${line:0:8}.name)"
     if [ ! -f "${FILE:17}" ]; then
         echo -e "$red $job_name has a file with an incorrect path:\n ----- ${FILE:17} does not exist$white"
+        echo "$FILE" >> baddies.yml
     fi
 done < paths.yml
+
+if [ -f baddies.yml ]; then
+  exit
+fi
