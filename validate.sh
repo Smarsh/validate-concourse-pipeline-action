e9836afb01418716167ff9b20b0cd7fb47ef6493
Abrar Mohammed <abrar.mohammed@smarsh.com>
2021-07-21 11:19:04 +0100

    adding pwd

--- validate.sh
+++ validate.sh
@@ -86,6 +86,7 @@ cat jobs.yml
 paste -d ","  names.yml unique_file_paths.yml > test.csv
 echo "test.csv"
 cat test.csv
+pwd
 
 # Using the delimiter it checkes if the file does not exist, and if it doesn't exits will then alert that the Job Name does not have the file_path, and will put and non existing file in the baddies.yml
 while IFS="," read -r name file; do
